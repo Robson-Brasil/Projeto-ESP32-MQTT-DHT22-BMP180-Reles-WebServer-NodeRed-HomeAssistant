39fa07b052226d74f71e40c696b280a263ee72c1
Robson-Brasil <Robson.Brasil@outlook.com>
2022-10-26 04:30:11 -0400

    Somente identação
    Signed-off-by: Robson-Brasil <Robson.Brasil@outlook.com>

--- ESP32-GoogleAssistant-Alexa-MQTT-NodeRed-HomeAssistant/ESP32-GoogleAssistant-Alexa-MQTT-NodeRed-HomeAssistant.ino
+++ ESP32-GoogleAssistant-Alexa-MQTT-NodeRed-HomeAssistant/ESP32-GoogleAssistant-Alexa-MQTT-NodeRed-HomeAssistant.ino
@@ -3,7 +3,7 @@ IoT - Automação Residencial
 Autor : Robson Brasil
 Dispositivo : ESP32 WROOM32
 Preferences--> Aditional boards Manager URLs: 
-                                   http://arduino.esp8266.com/stable/package_esp8266com_index.json,https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+                                    http://arduino.esp8266.com/stable/package_esp8266com_index.json,https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
 Download Board ESP32 (2.0.3):
 WiFi Manager
 Broker MQTT
