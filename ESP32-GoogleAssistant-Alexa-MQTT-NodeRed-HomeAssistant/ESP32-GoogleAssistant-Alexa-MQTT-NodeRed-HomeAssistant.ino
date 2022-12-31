6668952875d3ad32d59e5ae9e6ef6bbee020adab
Robson Brasil <robson.brasil@outlook.com>
2022-12-30 20:34:03 -0400

    Correção de erros no acionamento dos botões no html do webServer

--- ESP32-GoogleAssistant-Alexa-MQTT-NodeRed-HomeAssistant/ESP32-GoogleAssistant-Alexa-MQTT-NodeRed-HomeAssistant.ino
+++ ESP32-GoogleAssistant-Alexa-MQTT-NodeRed-HomeAssistant/ESP32-GoogleAssistant-Alexa-MQTT-NodeRed-HomeAssistant.ino
@@ -56,7 +56,7 @@ float diff = 1.0;
 
 unsigned long delayTime;
 
-#define ID_MQTT "ESP32-IoT" /* ID MQTT (para identificação de seção)           \
+#define ID_MQTT "ESP32-IoT-1" /* ID MQTT (para identificação de seção)           \
                             IMPORTANTE: Este deve ser único no broker (ou seja, \
                             se um client MQTT tentar entrar com o mesmo         \
                             ID de outro já conectado ao broker, o broker        \
@@ -132,97 +132,15 @@ const char* PARAM_INPUT_2 = "state";
 // Configuração da Porta Usada Pelo AsyncWebServer
 AsyncWebServer server(80);
 
-// Configuração HTML
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE html>
-<html lang="pt-br"></html>
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><style data-merge-styles="true"></style>
-  <title>ESP32 Web Server</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="/data/logo-1.png">
-  <style>
-    html {font-family: Arial; display: inline-block; text-align: center;}
-    h2 {font-size: 3.0rem;}
-    p {font-size: 3.0rem;}
-    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
-    .switch {position: relative; display: inline-block; width: 100px; height: 50px} 
-    .switch input {display: none}
-    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: rgb(255, 0, 0); border-radius: 6px}
-    .slider:before {position: absolute; content: ""; height: 32px; width: 32px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
-    input:checked+.slider {background-color: #00ff11}
-    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
-  </style>
-</head>
-<body>
-  <meta charset="utf-8">
-  <center><h2>Automação Residencial</h2>></center> 
-  <center><h2>Com ESP32 - IoT</h2></center>
+const char login_html[] PROGMEM = R"rawliteral(
 
- <style type="text/css">
-.tg  {border-collapse:collapse;border-color:#9ABAD9;border-spacing:0;}
-.tg td{background-color:#EBF5FF;border-color:#9ABAD9;border-style:solid;border-width:1px;color:#444;
-  font-family:Arial, sans-serif;font-size:14px;overflow:hidden;padding:10px 5px;word-break:normal;}
-.tg th{background-color:#409cff;border-color:#9ABAD9;border-style:solid;border-width:1px;color:rgb(255, 255, 255);
-  font-family:Arial, sans-serif;font-size:14px;font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
-.tg .tg-c3ow{border-color:inherit;text-align:center;vertical-align:top}
-.tg .tg-fbeb{background-color:#fff;border-color:#fff;color:#333333;text-align:left;vertical-align:top}
-.tg .tg-i91a{border-color:inherit;color:#000000;text-align:center;vertical-align:center}
-.tg .tg-0pky{border-color:inherit;text-align:center;vertical-align:center}
-.tg .tg-i91b{border-color:inherit;color:#020000;text-align:center;vertical-align:center}
-.tg .tg-1pky{border-color:inherit;text-align:center;vertical-align:center}
-
-</style>
-<table class="tg">
-<thead>
-  <tr>
-    <th class="tg-fbeb"></th>
-    <th class="tg-c3ow" colspan="2">BANCADA</th>
-    <th class="tg-c3ow" colspan="2">BANCADA</th>
-    <th class="tg-c3ow" colspan="2">Interruptor</th>
-    <th class="tg-c3ow" colspan="2">Interruptor</th>
-  </tr>
-</thead>
-<tbody>
-  <tr>
-    <td class="tg-i91a"><h4>Conjunto 1</h4></td>
-    <td class="tg-0pky" colspan="2"><h4>Luz Forte</h4><label class="switch"><input type="checkbox" onchange="toggleCheckbox(this)" id="23"><span class="slider"></span></label></td>
-    <td class="tg-0pky" colspan="2"><h4>Luz Fraca</h4><label class="switch"><input type="checkbox" onchange="toggleCheckbox(this)" id="22"><span class="slider"></span></label></td>
-    <td class="tg-0pky" colspan="2"><h4>Interruptor 3</h4><label class="switch"><input type="checkbox" onchange="toggleCheckbox(this)" id="21"><span class="slider"></span></label></td>
-    <td class="tg-0pky" colspan="2"><h4>Interruptor 4</h4><label class="switch"><input type="checkbox" onchange="toggleCheckbox(this)" id="19"><span class="slider"></span></label></td>
-    </tr>
-    <thead>
-      <tr>
-        <th class="tg-fbeb"></th>
-        <th class="tg-c3ow" colspan="2">Interruptor</th>
-        <th class="tg-c3ow" colspan="2">BLUETOOTH</th>
-        <th class="tg-c3ow" colspan="2">Interruptor</th>
-        <th class="tg-c3ow" colspan="2">QUARTO</th>
-      </tr>
-    </thead>
-    <tr> 
-    <td class="tg-i91b"><h4>Conjunto 2</h4></td>
-    <td class="tg-1pky" colspan="2"><h4>Interruptor 5</h4><label class="switch"><input type="checkbox" onchange="toggleCheckbox(this)" id="18"><span class="slider"></span></label></td>
-    <td class="tg-1pky" colspan="2"><h4>Som Bluetooth</h4><label class="switch"><input type="checkbox" onchange="toggleCheckbox(this)" id="5"><span class="slider"></span></label></td>
-    <td class="tg-1pky" colspan="2"><h4>Interruptor 7</h4><label class="switch"><input type="checkbox" onchange="toggleCheckbox(this)" id="25"><span class="slider"></span></label></td>
-    <td class="tg-1pky" colspan="2"><h4>Luz do Quarto</h4><label class="switch"><input type="checkbox" onchange="toggleCheckbox(this)" id="26"><span class="slider"></span></label></td>
-    </tr>
-        
-</tr></tbody>
-</table>
-  
 <script>function toggleCheckbox(element) {
   var xhr = new XMLHttpRequest();
-  if(element.checked){ xhr.open("GET", "/update?output="+element.id+"&state=1", true); }
-  else { xhr.open("GET", "/update?output="+element.id+"&state=0", true); }
+  if(element.checked){ xhr.open("GET", "/update?output="+element.id+"&state=0", true); }
+  else { xhr.open("GET", "/update?output="+element.id+"&state=1", true); }
   xhr.send();
 }
 </script>
-
-</center>
-</script>
-</body>
-</head>
-</html>
 )rawliteral";
 
 // Configuração dos Botões Usados
@@ -230,43 +148,43 @@ String processor(const String& var){
   //Serial.println(var);
   if(var == "BUTTONPLACEHOLDER1"){
     String buttons = "";
-    buttons += "<h4>Luz Forte</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"21\" " + outputState(21) + "><span class=\"slider\"></span></label>";
+    buttons += "<h4>Luz Forte</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"23\" " + outputState(23) + "><span class=\"slider\"></span></label>";
     return buttons;
   }
    if(var == "BUTTONPLACEHOLDER2"){
     String buttons = "";
-    buttons += "<h4>Luz Fraca</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"12\" " + outputState(12) + "><span class=\"slider\"></span></label>";
+    buttons += "<h4>Luz Fraca</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"22\" " + outputState(22) + "><span class=\"slider\"></span></label>";
      return buttons;
   }
    if(var == "BUTTONPLACEHOLDER3"){
     String buttons = "";
-    buttons += "<h4>Interruptor 3</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"14\" " + outputState(14) + "><span class=\"slider\"></span></label>";
+    buttons += "<h4>Interruptor 3</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"21\" " + outputState(21) + "><span class=\"slider\"></span></label>";
      return buttons;
   }
    if(var == "BUTTONPLACEHOLDER4"){
     String buttons = "";
-    buttons += "<h4>Interruptor 4</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"27\" " + outputState(27) + "><span class=\"slider\"></span></label>";
+    buttons += "<h4>Interruptor 4</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"19\" " + outputState(19) + "><span class=\"slider\"></span></label>";
      return buttons;
   }
 
   if(var == "BUTTONPLACEHOLDER5"){
     String buttons = "";
-    buttons += "<h4>Interruptor 5</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"26\" " + outputState(26) + "><span class=\"slider\"></span></label>";
+    buttons += "<h4>Interruptor 5</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"18\" " + outputState(18) + "><span class=\"slider\"></span></label>";
     return buttons;
   }
    if(var == "BUTTONPLACEHOLDER6"){
     String buttons = "";
-    buttons += "<h4>Som Bluetooth</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"25\" " + outputState(25) + "><span class=\"slider\"></span></label>";
+    buttons += "<h4>Som Bluetooth</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"5\" " + outputState(5) + "><span class=\"slider\"></span></label>";
      return buttons;
   }
    if(var == "BUTTONPLACEHOLDER7"){
     String buttons = "";
-    buttons += "<h4>Interruptor 7</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"33\" " + outputState(33) + "><span class=\"slider\"></span></label>";
+    buttons += "<h4>Interruptor 7</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"25\" " + outputState(25) + "><span class=\"slider\"></span></label>";
      return buttons;
   }
    if(var == "BUTTONPLACEHOLDER8"){
     String buttons = "";
-    buttons += "<h4>Luz do Quarto</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"32\" " + outputState(32) + "><span class=\"slider\"></span></label>";
+    buttons += "<h4>Luz do Quarto</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"26\" " + outputState(26) + "><span class=\"slider\"></span></label>";
      return buttons;
   }
    
@@ -301,21 +219,20 @@ void setup() {
   initMQTT();
   dht.begin();
 
-  SPIFFS.begin();
-  //Envia o arquivo html quando entrar quando fizerem uma requisição na raiz do servidor
-  server.serveStatic("/", SPIFFS, "/data/IoT-Internet-das-Coisas.html");
- 
+  if(!SPIFFS.begin(true)){
+      Serial.println("An Error has occurred while mounting SPIFFS");
+      return;
+    }
+    
   // Print do IP Local do ESP32
   Serial.println(WiFi.localIP());
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     if(!request->authenticate(http_username, http_password))
       return request->requestAuthentication();
-    request->send_P(200, "text/html", index_html, processor);
+      request->send(SPIFFS, "/WebServer.html", String(), false, processor);
   });
-    
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });
+
+  server.serveStatic("/", SPIFFS, "/");
 
   // Send a GET request to <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
   server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
