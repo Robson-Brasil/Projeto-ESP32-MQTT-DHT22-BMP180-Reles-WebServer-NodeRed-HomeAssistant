80f3009d0547eef271cbc415d6f8ad218522f28d
Robson Brasil <robson.brasil@outlook.com>
2023-06-08 18:34:28 -0400

    Tentativa de correção da Dashboard do WebServer

--- Projeto-ESP32-MQTT-DHT22-Reles-WebServer-NodeRed-HomeAssistant/data/darkmode.js
+++ Projeto-ESP32-MQTT-DHT22-Reles-WebServer-NodeRed-HomeAssistant/data/darkmode.js
@@ -1,29 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    const switchButtons = document.querySelectorAll('.switch-button');
 
-  function toggleCheckbox(element) {
-    var xhr = new XMLHttpRequest();
-    if (element.checked) { xhr.open("GET", "/update?relay=" + element.id + "&state=1", true); }
-    else { xhr.open("GET", "/update?relay=" + element.id + "&state=0", true); xhr.send(); }
-    xhr.send();
-}    
+    darkModeToggle.addEventListener('change', () => {
+        document.body.classList.toggle('dark-mode');
+    });
 
-
-  const darkModeSwitch = document.getElementById("dark-mode-switch");
-  const darkModeImg = document.querySelector(".dark-mode-image");
-  const lightModeImg = document.querySelector(".light-mode-image");
-  const body = document.querySelector('body');
-
-  body.classList.add('light-mode'); // adiciona a classe 'light-mode' ao body quando a página é carregada
-
-  darkModeSwitch.addEventListener('click', function () {
-    body.classList.toggle('dark-mode');
-    if (body.classList.contains('dark-mode')) {
-      lightModeImg.style.display = 'inline-block'; // exibe a imagem do modo claro
-      darkModeImg.style.display = 'none'; // oculta a imagem do modo escuro
-    } else {
-      lightModeImg.style.display = 'none'; // oculta a imagem do modo claro
-      darkModeImg.style.display = 'inline-block'; // exibe a imagem do modo escuro
-    }
-  });
-
+    switchButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            button.classList.toggle('active');
+        });
+    });
 });
