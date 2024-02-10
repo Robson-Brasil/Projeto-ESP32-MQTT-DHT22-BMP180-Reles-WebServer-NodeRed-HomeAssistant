9498fb5f424f513f1160384d79d645fb1fd48595
Robson Brasil <robson.brasil@outlook.com>
2024-02-10 09:01:08 -0400

    Troca dos nomes dos tópicos MQTT
    Foram feitas as trocas dos Nomes dos Subs de Comando para Estado e dos Publ de Estado para Comando, entendo que assim fique correto a nomenclatura dos tópicos

--- Projeto-ESP32-MQTT-DHT22-Reles-WebServer-NodeRed-HomeAssistant/TopicosMQTT.h
+++ Projeto-ESP32-MQTT-DHT22-Reles-WebServer-NodeRed-HomeAssistant/TopicosMQTT.h
@@ -1,14 +1,14 @@
 // Tópicos do Subscribe
 //Módulo Relé de 8 Canais
-const char* sub0 = "ESP32/MinhaCasa/QuartoRobson/Ligar-DesligarTudo/Comando"; // Somente por MQTT
-const char* sub1 = "ESP32/MinhaCasa/QuartoRobson/Interruptor1/Comando";       // Ligados ao Nora/MQTT
-const char* sub2 = "ESP32/MinhaCasa/QuartoRobson/Interruptor2/Comando";       // Ligados ao Nora/MQTT
-const char* sub3 = "ESP32/MinhaCasa/QuartoRobson/Interruptor3/Comando";       // Ligados ao Nora/MQTT
-const char* sub4 = "ESP32/MinhaCasa/QuartoRobson/Interruptor4/Comando";       // Ligados ao Nora/MQTT
-const char* sub5 = "ESP32/MinhaCasa/QuartoRobson/Interruptor5/Comando";       // Ligados ao Nora/MQTT
-const char* sub6 = "ESP32/MinhaCasa/QuartoRobson/Interruptor6/Comando";       // Ligados ao MQTT/Alexa
-const char* sub7 = "ESP32/MinhaCasa/QuartoRobson/Interruptor7/Comando";       // Ligados ao MQTT/Alexa
-const char* sub8 = "ESP32/MinhaCasa/QuartoRobson/Interruptor8/Comando";       // Ligados ao MQTT/Alexa
+const char* sub0 = "ESP32/MinhaCasa/QuartoRobson/Ligar-DesligarTudo/Estado"; // Somente por MQTT
+const char* sub1 = "ESP32/MinhaCasa/QuartoRobson/Interruptor1/Estado";       // Ligados ao Nora/MQTT
+const char* sub2 = "ESP32/MinhaCasa/QuartoRobson/Interruptor2/Estado";       // Ligados ao Nora/MQTT
+const char* sub3 = "ESP32/MinhaCasa/QuartoRobson/Interruptor3/Estado";       // Ligados ao Nora/MQTT
+const char* sub4 = "ESP32/MinhaCasa/QuartoRobson/Interruptor4/Estado";       // Ligados ao Nora/MQTT
+const char* sub5 = "ESP32/MinhaCasa/QuartoRobson/Interruptor5/Estado";       // Ligados ao Nora/MQTT
+const char* sub6 = "ESP32/MinhaCasa/QuartoRobson/Interruptor6/Estado";       // Ligados ao MQTT/Alexa
+const char* sub7 = "ESP32/MinhaCasa/QuartoRobson/Interruptor7/Estado";       // Ligados ao MQTT/Alexa
+const char* sub8 = "ESP32/MinhaCasa/QuartoRobson/Interruptor8/Estado";       // Ligados ao MQTT/Alexa
 
 //Sensor DHT22 (Temperatura e Umidade)
 const char* sub9  = "ESP32/MinhaCasa/QuartoRobson/Temperatura";               // Somente por MQTT
@@ -27,15 +27,15 @@ const char* sub17 = "ESP32/MinhaCasa/QuartoRobson/BMP180/AltitudeNivelMar";     
 
 // Tópicos do Publish
 //Módulo Relé de 8 Canais
-const char* pub0 = "ESP32/MinhaCasa/QuartoRobson/Ligar-DesligarTudo/Estado";  // Somente por MQTT
-const char* pub1 = "ESP32/MinhaCasa/QuartoRobson/Interruptor1/Estado";        // Ligados ao Nora/MQTT
-const char* pub2 = "ESP32/MinhaCasa/QuartoRobson/Interruptor2/Estado";        // Ligados ao Nora/MQTT
-const char* pub3 = "ESP32/MinhaCasa/QuartoRobson/Interruptor3/Estado";        // Ligados ao Nora/MQTT
-const char* pub4 = "ESP32/MinhaCasa/QuartoRobson/Interruptor4/Estado";        // Ligados ao Nora/MQTT
-const char* pub5 = "ESP32/MinhaCasa/QuartoRobson/Interruptor5/Estado";        // Ligados ao Nora/MQTT
-const char* pub6 = "ESP32/MinhaCasa/QuartoRobson/Interruptor6/Estado";        // Ligados ao MQTT/Alexa
-const char* pub7 = "ESP32/MinhaCasa/QuartoRobson/Interruptor7/Estado";        // Ligados ao MQTT/Alexa
-const char* pub8 = "ESP32/MinhaCasa/QuartoRobson/Interruptor8/Estado";        // Ligados ao MQTT/Alexa
+const char* pub0 = "ESP32/MinhaCasa/QuartoRobson/Ligar-DesligarTudo/Comando";  // Somente por MQTT
+const char* pub1 = "ESP32/MinhaCasa/QuartoRobson/Interruptor1/Comando";        // Ligados ao Nora/MQTT
+const char* pub2 = "ESP32/MinhaCasa/QuartoRobson/Interruptor2/Comando";        // Ligados ao Nora/MQTT
+const char* pub3 = "ESP32/MinhaCasa/QuartoRobson/Interruptor3/Comando";        // Ligados ao Nora/MQTT
+const char* pub4 = "ESP32/MinhaCasa/QuartoRobson/Interruptor4/Comando";        // Ligados ao Nora/MQTT
+const char* pub5 = "ESP32/MinhaCasa/QuartoRobson/Interruptor5/Comando";        // Ligados ao Nora/MQTT
+const char* pub6 = "ESP32/MinhaCasa/QuartoRobson/Interruptor6/Comando";        // Ligados ao MQTT/Alexa
+const char* pub7 = "ESP32/MinhaCasa/QuartoRobson/Interruptor7/Comando";        // Ligados ao MQTT/Alexa
+const char* pub8 = "ESP32/MinhaCasa/QuartoRobson/Interruptor8/Comando";        // Ligados ao MQTT/Alexa
 
 //Sensor DHT22 (Temperatura e Umidade)
 const char* pub9  = "ESP32/MinhaCasa/QuartoRobson/Temperatura";               // Somente por MQTT
